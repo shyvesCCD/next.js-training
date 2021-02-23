f1f7638d28db8e209b132aae36ddee2a371ecba4
Pedro Chaves <pedroochavess97@gmail.com>
2021-02-23 01:54:49 -0300

    server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,42 @@
-import { Title } from '../styles/pages/Home';
+import { GetServerSideProps } from "next";
+import { Title } from "../styles/pages/Home";
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+// TTFB -> tempo que demora pro primeiro byte estar disponivel para o frontend
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello World!</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map((recommendedProducts) => {
+            return (
+              <li key={recommendedProducts.id}>{recommendedProducts.title}</li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch("http://localhost:3333/recommended");
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts,
+    },
+  };
+};
